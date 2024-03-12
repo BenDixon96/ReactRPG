9bab4d417c0e05d7b10311cc3ddd296ec6643378
Ben Dixon <benjdixon96@yahoo.co.uk>
2024-03-12 20:14:16 +0000

    nav cor

--- src/components/Scene/Explore.jsx
+++ src/components/Scene/Explore.jsx
@@ -1,21 +1,53 @@
 import './Explore.css'
+import currentLocation from './Nav_class/location';
+import React, { useEffect, useState } from 'react';
 
 
 
 const Explore = () => {
+   
+    const [count, setCount] = useState(0)
+    const [myLocation, setMyLocation] = useState(new currentLocation(0, 0))
+    
+    
+    const handleClick = (direction, value) => {
+        event.preventDefault();
+        
+        
+        
+
+        myLocation.move(direction, value)
+        setMyLocation(myLocation)
+        setCount(count + 1)
+     
+
+        
+        
+        
+        
+        
+    }
+
+    
+
+
+
+
+
     return(
         <div id="control">
         <h1>Explore </h1>
-        <button id="N">
+        <p> your location ({myLocation.xAxis}) ({myLocation.yAxis}) </p>
+        <button id="N" value={1} onClick={() => handleClick('N', 1)}>
             north
         </button>
-        <button id="E">
+        <button id="E" value={1}onClick={() => handleClick('E', -1)}>
             east
         </button>
-        <button id="S">
+        <button id="S" value={-1}onClick={() => handleClick('S', -1)}>
             south
         </button>
-        <button id="W">
+        <button id="W" value={-1}onClick={() => handleClick('W', -1)}>
             west
         </button>
     </div>
---
+++ src/components/Scene/Nav_class/location.js
@@ -0,0 +1,44 @@
+
+
+class currentLocation {
+    constructor(x, y){
+        this.xAxis = 0
+        this.yAxis = 0
+    }
+    updateLocation(xAxis, yAxis){
+        this.xAxis = xAxis
+        this.yAxis = yAxis
+    }
+    move(direction, movement){
+        if(direction === "S"){
+            
+            console.log(this.yAxis)
+            this.yAxis -= 1
+
+            console.log('south')
+            console.log(this.yAxis)
+            console.log(movement)  
+        }
+        else if( direction === "N"){
+            console.log("north")
+            this.yAxis += 1
+        }
+
+        else if( direction === "W"){
+            console.log("west")
+            this.xAxis -= 1
+        }
+        else if( direction === "E"){
+            console.log("east")
+            this.xAxis += 1
+        } 
+
+
+        }
+    }
+
+
+
+
+
+export default currentLocation
