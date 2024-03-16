c2e5356ba3be9e01b85c95ce5313dc49406d277b
Ben Dixon <benjdixon96@yahoo.co.uk>
2024-03-16 12:54:35 +0000

    added level UP!

--- src/Data/Player/playerChar.js
+++ src/Data/Player/playerChar.js
@@ -7,6 +7,8 @@ class playerChar {
         this.inventory = []
         this.weapon = null
         this.armour = null
+        this.level = 1
+        this.xp = 0
         
         
 
@@ -14,7 +16,31 @@ class playerChar {
     pickUp(item){
         this.inventory.push(item)
     }
-    
+    equipWeapon(weapon){
+        this.weapon = weapon
+    }
+    equipArmour(armour){
+        this.armour = armour
+    }
+    tillLevel(){
+        const requiredXP = this.level * 100
+
+        return requiredXP - this.xp
+    }
+    addXp(xp){
+        this.xp += xp
+        this.levelUp()
+    }
+    levelUp(){
+        const requiredXP = this.level * 100
+        if(this.xp >= this.tillLevel()){
+            
+            const remainderXP = this.xp - requiredXP
+            this.level += 1
+            this.xp = remainderXP
+            
+        }
+    }
 
 }
 
--- src/Data/Player/playerChar.test.js
+++ src/Data/Player/playerChar.test.js
@@ -10,12 +10,15 @@ describe('initalise a class', () => {
       expect(testPlayer.armour).toBe(null)
       expect(testPlayer.weapon).toBe(null)
       expect(testPlayer.inventory).toEqual([])
+      expect(testPlayer.level).toBe(1)
+      expect(testPlayer.xp).toBe(0)
+
 
     });
   });
 
   describe('test to pick up item', () => {
-    it('checks if class is intalised', () => {
+    it('checks picking up item', () => {
       const testPlayer = new playerChar("Bob")
       const testItem1 = 'stuff'
       const testItem2 = 'more stuff'
@@ -26,4 +29,66 @@ describe('initalise a class', () => {
 
     });
   });
+  describe('test to equip weapon', () => {
+    it('test equip weapon', () => {
+      const testPlayer = new playerChar("Bob")
+      const testWeapon = 'sword'
+      
 
+      testPlayer.equipWeapon(testWeapon)
+
+      
+      expect(testPlayer.weapon).toEqual('sword')
+
+    });
+  });
+  describe('test to equip armour', () => {
+    it('test equip armour', () => {
+      const testPlayer = new playerChar("Bob")
+      const testArmour = 'leather armour'
+      testPlayer.equipArmour(testArmour)
+      expect(testPlayer.armour).toEqual('leather armour')
+      
+    });
+  });
+  describe('get xp to level up', () => {
+    it('shows how much xp you need till you level up', () => {
+      const testPlayer = new playerChar("Bob")
+      
+      expect(testPlayer.tillLevel()).toEqual(100)
+      
+    });
+  });
+  describe('adding XP', () => {
+    it('adding XP to player', () => {
+      const testPlayer = new playerChar("Bob")
+      testPlayer.addXp(10)
+
+      
+      expect(testPlayer.xp).toBe(10)
+      
+    });
+  });
+  describe('Level UP!', () => {
+    it('Leveling up', () => {
+      const testPlayer = new playerChar("Bob")
+      testPlayer.addXp(100)
+
+      
+      expect(testPlayer.level).toBe(2)
+      
+    });
+  });
+  describe('Level UP! with more xp', () => {
+    it('should reset xp but keep remainder', () => {
+      const testPlayer = new playerChar("Bob")
+      testPlayer.addXp(110)
+
+      
+      expect(testPlayer.level).toBe(2)
+      expect(testPlayer.xp).toBe(10)
+      expect(testPlayer.tillLevel()).toEqual(190)
+
+      
+    });
+  });
