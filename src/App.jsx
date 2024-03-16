3ff79ed766f5c16184cb65351cde5125a5f261e6
Ben Dixon <benjdixon96@yahoo.co.uk>
2024-03-16 14:36:48 +0000

    rmv makersLogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Explore from "./components/Scene/Explore";
 import Welcome from "./components/Welcome";
-import makersLogo from "./assets/Makers-Logo.png";import "./App.css";
+
 
 function App() {
   return (
