c70923e05a42652ba62e3e7349de6649981a8d20
Ben Dixon <benjdixon96@yahoo.co.uk>
2024-03-16 12:26:50 +0000

    add item to inv

--- src/Data/Player/playerChar.js
+++ src/Data/Player/playerChar.js
@@ -4,8 +4,17 @@ class playerChar {
     constructor(name){
         this.name = name
         this.charClass = null
+        this.inventory = []
+        this.weapon = null
+        this.armour = null
+        
+        
 
     }
+    pickUp(item){
+        this.inventory.push(item)
+    }
+    
 
 }
 
--- src/Data/Player/playerChar.test.js
+++ src/Data/Player/playerChar.test.js
@@ -7,5 +7,23 @@ describe('initalise a class', () => {
       const testPlayer = new playerChar("Bob")
       expect(testPlayer).toBeDefined();
       expect(testPlayer.name).toBe("Bob")
+      expect(testPlayer.armour).toBe(null)
+      expect(testPlayer.weapon).toBe(null)
+      expect(testPlayer.inventory).toEqual([])
+
     });
   });
+
+  describe('test to pick up item', () => {
+    it('checks if class is intalised', () => {
+      const testPlayer = new playerChar("Bob")
+      const testItem1 = 'stuff'
+      const testItem2 = 'more stuff'
+
+      testPlayer.pickUp(testItem1)
+      testPlayer.pickUp(testItem2)
+      expect(testPlayer.inventory).toEqual(['stuff', 'more stuff'])
+
+    });
+  });
+
