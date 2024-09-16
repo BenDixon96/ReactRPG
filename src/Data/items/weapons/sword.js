42e043f30aae7d3c46a51c25fe8a368dbcd1e3c8
Ben Dixon <benjdixon96@yahoo.co.uk>
2024-09-16 15:05:57 +0100

    added weapon

---
+++ src/Data/items/weapons/sword.js
@@ -0,0 +1,5 @@
+import Weapon from "./weapon";
+
+const sword = new Weapon(1, "sword", 10)
+
+export default sword
---
+++ src/Data/items/weapons/weapon.js
@@ -0,0 +1,10 @@
+
+class Weapon{
+    constructor(id, name, damage){
+        this.id = id;
+        this.name = name;
+        this.damage = damage;
+        this.weapon = true;
+    }
+}
+export default Weapon
